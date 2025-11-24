36ff4de1baa2d4d008867adcc55d85f68bcea7b7
aa14126 <170713000+aa1412666@users.noreply.github.com>
2025-11-24 10:41:25 +0800

    feat: 新增激光雷达讨论页面，提供路由占位功能

---
+++ src/views/LidarDebate.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="lidar-debate">
+    <h2>激光雷达讨论（占位页）</h2>
+    <p>此页面用于演示路由占位。你可以在这里填充具体的业务内容与交互。</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LidarDebate'
+};
+</script>
+
+<style scoped>
+.lidar-debate {
+  padding: 20px;
+}
+</style>
